5054880b55a806b583b500305c60581ce525f712
Abdullah431002487 <aboodi331122@gmail.com>
2024-07-14 10:43:39 +0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    const timeElement = document.querySelector('#current-time');
+    timeElement.textContent = timeString;
+}
+
+
+updateTime();
+
+
+setInterval(updateTime, 1000);
